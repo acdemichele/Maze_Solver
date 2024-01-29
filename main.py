60bbec5678eee8cf4eaca55a2b90da70d2363b50
acdemichele <93853207+acdemichele@users.noreply.github.com>
2024-01-29 15:39:32 -0500

    third commit,  created Point and Line Class to facilitate drawing, added draw method to Line class, updated Window Contructor and added draw_line method to Window class

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 from tkinter import Tk, BOTH, Canvas
 
+
+
+class Point:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+class Line:
+    def __init__(self, pt1: Point, pt2: Point):
+        self.pt1 = pt1
+        self.pt2 = pt2
+    
+    def draw(self, canvas: Canvas, fill_color: str):
+        canvas.create_line(
+            self.pt1.x, self.pt1.y, self.pt2.x, self.pt2.y, fill=fill_color, width=2
+        )
+        self.canvas.pack(fill=BOTH, expand=True)
+        
+        
 class Window:
     def __init__(self, width, height):
         self.width = width
@@ -21,11 +39,17 @@ class Window:
             
     def close(self):
         self.running = False
-        
+    
+    def draw_line(self, line: Line, fill_color: str):
+        line.draw()
 
 def main():
     
     win = Window(800, 600)
+    pt1 = Point(100, 100)
+    pt2 = Point(400,400)
+    line = Line(pt1, pt2)
+    win.draw_line(line, "black")
     win.wait_for_close()
 
 main()
