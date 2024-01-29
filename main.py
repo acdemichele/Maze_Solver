61c2791133466bc4a5d6ef1f81b5363dfff5076a
acdemichele <93853207+acdemichele@users.noreply.github.com>
2024-01-29 15:06:56 -0500

    first commit, built Window Class and added redraw(), wait_for_close(), and close() methods

---
+++ main.py
@@ -0,0 +1,29 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.__root = Tk()
+        self.__root.title("Maze Solver")
+        self.canvas = Canvas(self.__root, width=self.width, height=self.height, bg="white")
+        self.running = False
+        self.canvas.pack(fill=BOTH, expand=True)
+        self.__root.protocol("WM_Delete_Window", self.close)
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+    
+    def wait_for_close(self):
+        self.running = True
+        while self.running:
+            self.redraw()
+            
+    def close(self):
+        self.running = False
+        
+
+def main():
+    
+    win = Window(800, 600)
+    win.wait_for_close()
